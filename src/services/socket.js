ed64781e8559d90dcc1e930a59495e2d04045851
mascarenhaswanja <wanjamarly.deoliveiramascarenhas@georgebrown.ca>
2020-04-27 12:01:52 -0400

    error Heroku

--- src/services/socket.js
+++ src/services/socket.js
@@ -2,11 +2,6 @@ import socketIOClient from 'socket.io-client';
 
 import { baseURL } from './baseURL.json';
 
-//const socket = socketIOClient(baseURL);
-
-const socket = socketIOClient(baseURL, {
-    secure: true,
-    transports: ['websocket'],
-  });
+const socket = socketIOClient(baseURL);
 
 export default socket;
